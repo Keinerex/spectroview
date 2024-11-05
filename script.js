37eda9d2722a17e0b78311653d365324838e3081
Юрков Владислав Юрьевич <urkov.v@skbkontur.ru>
2024-11-05 23:39:43 +0700

    ветка для pages

--- script.js
+++
@@ -1,7 +0,0 @@
-const table = document.getElementById("table");
-
-const rows = table.children;
-
-Array.from(table.rows).forEach(row => {
-    console.log(Array.from(row.cells).map(cell => cell.textContent))
-})
--- src/components/Header/Header.tsx
+++
@@ -1,7 +0,0 @@
-export const Header = () => {
-  return (
-    <header className="w-full bg-red-400 h-12">
-
-    </header>
-  );
-};
